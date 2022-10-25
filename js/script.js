ce3bff97af3f9ffe4f619817a919410ca3cd8a02
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2022-10-25 18:48:42 +0330

    changing function into interval

--- js/script.js
+++ js/script.js
@@ -5,19 +5,19 @@ const minuteFiled = $.querySelector("#minute");
 const secondFiled = $.querySelector("#seconds");
 
 // functions
-function setTime() {
-  setInterval(() => {
-    hourFiled.innerHTML=new Date().getHours();
-    minuteFiled.innerHTML = new Date().getMinutes();
-    secondFiled.innerHTML = new Date().getSeconds();
+setInterval(() => {
+    let hours=new Date().getHours()
+    let minute=new Date().getMinutes()
+    let second=new Date().getSeconds()
+    hourFiled.innerHTML=hours
+    minuteFiled.innerHTML=minute
+    secondFiled.innerHTML=second
     if (minuteFiled.innerHTML<10) {
-        minuteFiled.innerHTML=`0${new Date().getMinutes()}`
+        minuteFiled.innerHTML=`0${minute}`
     }
     if (secondFiled.innerHTML<10) {
-        secondFiled.innerHTML=`0${new Date().getSeconds()}`
-    }
-  }, 1000);
-}
-
+        secondFiled.innerHTML=`0${second}`
+    }    
+}, 1000);
 // event Listeners
-window.addEventListener("load", setTime);
+
